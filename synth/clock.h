adf1a7cea48e8b5c7452b70cf32bb44d0c88b816
Nick Allott <neo_nmik@hotmail.com>
2023-09-05 16:27:43 +0100

    Refactored Divison calculation

--- synth/clock.h
+++ synth/clock.h
@@ -44,7 +44,7 @@ namespace CLOCK {
 
         void calculate_division (void) {
             // calculation for division using samples per bar, and then using the division from there.
-            _samples_per_division = (((60 * _sample_rate) / (_bpm / _beats)) / _division);
+            _samples_per_division = ((((60 * _sample_rate) << 2) /_bpm) / _division);
 
 
         }
