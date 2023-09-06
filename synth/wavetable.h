139379ae3d840540834516366880c74014cb1d5f
Nick Allott <neo_nmik@hotmail.com>
2023-09-06 18:01:37 +0100

    Added interpolation

--- synth/wavetable.h
+++ synth/wavetable.h
@@ -1917,19 +1917,19 @@ const int16_t wavetable[] = {
     -1559, -7294, 3428, -10777, -16647
 };
 
-inline int16_t get_wavetable  (uint32_t index, uint16_t vector) {
-    // volatile uint16_t _index = index >> 12;
-    // volatile uint16_t frac = index & 0xFFF;
+inline int16_t getWavetable  (uint32_t index, uint16_t vector) {
+    volatile uint16_t _index = index >> 12;
+    volatile uint16_t frac = index & 0xFFF;
 
-    // // Get the two neighboring samples from the wavetable
-    // volatile int16_t sample1 = wavetable[_index];
-    // volatile int16_t sample2 = wavetable[(_index + 1) & 0xFF];
+    // Get the two neighboring samples from the wavetable
+    volatile int16_t sample1 = wavetable[(_index) + vector];
+    volatile int16_t sample2 = wavetable[((_index + 1) & 0xFF) + vector];
 
-    // // Linear interpolation
-    // volatile int16_t interpolatedSample = sample1 + ((sample2 - sample1) * frac >> 12);
+    // Linear interpolation
+    volatile int16_t interpolatedSample = sample1 + ((sample2 - sample1) * frac >> 12);
     
-    // return interpolatedSample;
+    return interpolatedSample;
     
     // un-interpolated output
-    return wavetable[index + vector];
+    // return wavetable[index + vector];
 }
