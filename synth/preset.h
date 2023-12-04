e9379a195981ff0dfdf877b4724b25454e542f7b
Nick Allott <neo_nmik@hotmail.com>
2023-12-04 20:28:56 +0000

    Fleshed out new Preset outline

--- synth/preset.h
+++ synth/preset.h
@@ -17,23 +17,14 @@ struct PRESET {
     };
     struct Lfo {
         bool state = false;
-        // 1 byte padding
 
         uint16_t matrix = 0;
         uint16_t rate = 0;
         uint16_t depth = 0;
         uint16_t wave = 0;
-
-        // commented until they're worked into the Preset saving layout
-        
-        // uint16_t attack = 2;
-        // uint16_t decay = 2;
-        // uint16_t sustain = 1023;
-        // uint16_t release = 10;
     };
     struct Arp {
         bool state = false;
-        // 1 byte padding
 
         uint16_t gate = 0;
         uint16_t divisions = 511;
@@ -41,13 +32,12 @@ struct PRESET {
         uint16_t direction = 0;
 
         uint16_t rest = 0;
-        uint16_t bpm = 289; // 120bpm
+        uint16_t bpm = 289;
         uint16_t fMode = 0;
         uint16_t octMode = 0;
     };
     struct Flt {
         bool state = true;
-        // 1 byte padding
         
         uint16_t cutoff = 1023;
         uint16_t resonance = 0;
@@ -62,6 +52,7 @@ struct PRESET {
     struct Fx {
         uint16_t gain = 0;
     };
+
     Wav Wave;
     Env Envelope;
     Lfo Modulation;
@@ -72,16 +63,19 @@ struct PRESET {
 
 
 struct EnvelopeData {
-    bool state = false;
-    
     uint16_t attack = 2;
     uint16_t decay = 2;
     uint16_t sustain = 1023;
     uint16_t release = 10;
-
-    uint16_t padding[4];
 };
 
+struct StateData {
+    bool    stateLFO = false;
+    bool    stateFLT = false;
+    bool    stateARP = false;
+    
+    bool    padding = false;
+};
 struct OscillatorData {
     uint16_t waveshape = 0;
     uint16_t vector = 0;
@@ -94,9 +88,6 @@ struct OscillatorData {
 };
 
 struct LfoData {
-    bool state = false;
-    // 1 byte padding
-
     uint16_t matrix = 0;
     uint16_t rate = 0;
     uint16_t depth = 0;
@@ -109,9 +100,6 @@ struct LfoData {
 };
 
 struct FilterData {
-    bool state = true;
-    // 1 byte padding
-    
     uint16_t cutoff = 1023;
     uint16_t resonance = 0;
     uint16_t punch = 0;
@@ -123,9 +111,6 @@ struct FilterData {
 };
 
 struct ArpeggiatorData {
-    bool state = false;
-    // 1 byte padding
-
     uint16_t gate = 0;
     uint16_t divisions = 511;
     uint16_t range = 0;
@@ -146,8 +131,10 @@ struct FxData {
 };
 
 struct NEW_PRESET {
-    uint8_t             startbit;
     uint8_t             version; // for checking preset version
+    uint8_t             length;
+
+    StateData           States;
 
     OscillatorData      Wave;
     LfoData             Modulation;
@@ -155,5 +142,23 @@ struct NEW_PRESET {
     ArpeggiatorData     Arpeggiator;
     FxData              Effects;
 
-    uint8_t             endbit;
+    uint8_t             padding[10];
 };
+
+inline void printPresetSizing (void) {
+    printf("\nSize of ");
+    printf("Old Preset: %04d bytes\n\n", sizeof(PRESET));
+    
+    printf("Size of ");
+    printf("New Preset: %04d bytes\n", sizeof(NEW_PRESET));
+    printf("    Size of ");
+    printf("Oscillator Data: %04d bytes\n", sizeof(OscillatorData));
+    printf("    Size of ");
+    printf("LFO Data: %04d bytes\n", sizeof(LfoData));
+    printf("    Size of ");
+    printf("Filter Data: %04d bytes\n", sizeof(FilterData));
+    printf("    Size of ");
+    printf("Arp Data: %04d bytes\n", sizeof(ArpeggiatorData));
+    printf("    Size of ");
+    printf("FX Data: %04d bytes\n\n", sizeof(FxData)); 
+}
